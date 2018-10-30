4960f55bd4130066ce5c999bbb650136cb8641bc
Theesit (Jo) Konkaew <kaorism@gmail.com>
2018-10-30 21:20:52 +0700

    Add property testing

--- test/proper_poker_test.exs
+++ test/proper_poker_test.exs
@@ -1,5 +1,7 @@
 defmodule ProperPokerTest do
   use ExUnit.Case
+  use ExUnitProperties
+
   doctest ProperPoker
 
   alias PokerHands
@@ -9,5 +11,22 @@ defmodule ProperPokerTest do
       assert ProperPoker.vs("2H 3D 5S 9C KD", "2C 3H 4S 8C KH") ==
                PokerHands.vs("2H 3D 5S 9C KD", "2C 3H 4S 8C KH")
     end
+
+    property "always return same result as original" do
+      check all left_hand_cards <- StreamData.list_of(StreamData.member_of(deck()), length: 5),
+                right_hand_cards <- StreamData.list_of(StreamData.member_of(deck()), length: 5) do
+        left_hand = left_hand_cards |> Enum.join(" ")
+        right_hand = right_hand_cards |> Enum.join(" ")
+
+        assert ProperPoker.vs(left_hand, right_hand) == PokerHands.vs(left_hand, right_hand)
+      end
+    end
+  end
+
+  defp deck do
+    suits = ["C", "D", "H", "S"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+
+    for value <- values, suit <- suits, do: value <> suit
   end
 end
