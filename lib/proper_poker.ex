99015d540ea45cb57dcb185a3e4aab325348a7f9
Theesit (Jo) Konkaew <kaorism@gmail.com>
2018-10-30 19:45:17 +0700

    init

---
+++ lib/proper_poker.ex
@@ -0,0 +1,18 @@
+defmodule ProperPoker do
+  @moduledoc """
+  Documentation for ProperPoker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProperPoker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/proper_poker_test.exs
@@ -0,0 +1,8 @@
+defmodule ProperPokerTest do
+  use ExUnit.Case
+  doctest ProperPoker
+
+  test "greets the world" do
+    assert ProperPoker.hello() == :world
+  end
+end
